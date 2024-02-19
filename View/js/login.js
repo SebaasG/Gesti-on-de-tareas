9f5ded5a5295d91f55a1986f11a15b0171ae8800
Sebastian <miguel.poveda1220@gmail.com>
2024-02-19 16:19:39 -0500

    alertas

--- View/js/login.js
+++ View/js/login.js
@@ -1,21 +1,50 @@
-const boton  = document.getElementById('btn')
-async function login(){
+const boton = document.getElementById('btn')
+async function login() {
     const user = document.getElementById('nombre').value;
     const pass = document.getElementById('pas').value;
-console.log(user, pass)
+    console.log(user, pass)
 
-await fetch(`http://localHost:1234/user/login/${user}/${pass}`)
-.then(response =>{
-    if(response.ok){
-        alert('bienvenido')
-        window.location.href = '../../View/hola.html'
-    }else{
-        alert('papi sea serio')
-    }
-})
+    await fetch(`http://localHost:1234/user/login/${user}/${pass}`)
+        .then(response => {
+            if (response.ok) {
+                Swal.fire({
+                    position: 'center',
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    allowEnterKey: false,
+                    stopKeydownPropagation: true,
+                    title: '¡Éxito!',
+                    html: `<b class="texto-alerta">BIENVENIDO ${user} </b>`,
+                    icon: 'success',
+                    iconColor: '#00003d',
+                    showConfirmButton: false, // Esta línea es la que se ha añadido
+                    timer: 8000
+                });
+                setTimeout(() => {
+                    location.href = '../../View/hola.html';
+                }, 3000);
+
+            } else {
+                Swal.fire({
+                    position: 'center',
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    allowEnterKey: false,
+                    stopKeydownPropagation: true,
+                    title: '¡ ERROR !',
+                    html: '<b class="texto-alerta"> ¡Usuario o contraseña incorrecta! </b>',
+                    icon: 'error',
+                    iconColor: '#00003d',
+                    confirmButtonColor: '#00003d',
+                    timer: 8000
+                });
+                // alet('papi sea serio')
+            }
+        })
 }
 
-boton.addEventListener('click',()=>{
+boton.addEventListener('click', () => {
     login()
-    })
+
+})
 
