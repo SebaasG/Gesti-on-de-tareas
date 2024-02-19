9b697e4f86af234b1bc6d2a45a3ed6fe5737fab8
Sebastian <miguel.poveda1220@gmail.com>
2024-02-19 16:03:14 -0500

    styles....

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-import express from 'express'
+import express, {json} from  'express'
 import {createUserRoutes} from './Routes/UserRoutes.js'
 import { usuarioModel } from './Model/UsuarioModel.js';
 import { corsMiddleware } from './Middleware/Cors.js';
-import bodyParser from 'body-parser';
+
 
 const app = express();
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json())
 app.use(corsMiddleware())
 app.use('/user',createUserRoutes({usuarioModel:usuarioModel}) )
 const PORT  = process.env.PORT || 1234
