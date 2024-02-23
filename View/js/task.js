fda1e7209eabc14007a077400688cd0e81287844
Sebastian <miguel.poveda1220@gmail.com>
2024-02-23 15:46:16 -0500

    asldsada

--- View/js/task.js
+++ View/js/task.js
@@ -6,15 +6,56 @@ const btnCreate = document.getElementById('createTask');//Boton para crear una n
 
 const btnCerrar = document.getElementById('cerrarSesion');
 
+// const modal = document.getElementById('miModal')
+// const modalHeader = modal.querySelector('modal-header')
 
 let contador = 0
+// async function getTask() {
+//     const docUser = await getDocUser();
+//     const response = await fetch('http://localHost:1234/task/get/' + docUser)
+//     const datos = await response.json()
+
+//     datos.forEach(datos => {
+//         contador = contador + 1
+//         const formattedDate = newDate(datos.dateStart)
+//         let div = document.createElement('div');
+//         div.innerHTML = `
+//             <div id = 'tarjet${contador}' class="tarjet" data-bs-toggle="modal" data-bs-target="#miModal">
+//             <div class="contenido"> 
+//             <h4>${datos.nameTask}</h4> <br>
+//             <p>Tarea: #${datos.numTask} </p>
+//             <p>categoria: ${datos.cateTask} </p>
+//             <p>Fecha de creacion: ${formattedDate}</p>
+//             <p>Estado: ${datos.stateTask}</p>
+//             </div>
+//         </div> `;
+//         document.getElementById('container').appendChild(div);
+//         // Añadir evento click para cada tarjeta creada
+//         document.getElementById('tarjet${contador}').addEventListener('click', function () {
+//             // aquí puedes ejecutar una función cuando se hace clic en la tarjeta
+//             console.log('Se hizo clic en la tarjeta:', datos);
+//         });
+//     });
+// };
+
+
+
+async function getDocUser() {
+    const data = await fetch('http://localHost:1234/task/' + nameUser)
+    const datos = await data.json()
+    const docUser = datos[0].docUser
+    return docUser
+
+}
+
+//Funcion para guardar tareas
 async function getTask() {
-    const docUser = await getDocUser(); 
-    const response = await fetch('http://localHost:1234/task/get/'+docUser)
+    const docUser = await getDocUser();
+    const response = await fetch('http://localHost:1234/task/get/' + docUser)
     const datos = await response.json()
-  
+
     datos.forEach(datos => {
-        contador = contador+1
+        contador = contador + 1
         const formattedDate = newDate(datos.dateStart)
         let div = document.createElement('div');
         div.innerHTML = `
@@ -28,75 +69,39 @@ async function getTask() {
             </div>
         </div> `;
         document.getElementById('container').appendChild(div);
+        // Añadir evento click para cada tarjeta creada
+        document.querySelector('#tarjet' + contador).addEventListener('click', function () {
+            // aquí puedes ejecutar una función cuando se hace clic en la tarjeta
+            llenarModal(datos.numTask)
+        });
     });
-
 };
 
+async function llenarModal(num) {
 
-async function crearModal(){
-    const docUser = await getDocUser(); 
-    const response = await fetch('http://localHost:1234/task/doc/'+docUser)
+    const response = await fetch('http://localHost:1234/task/doc/' + num);
     const datos = await response.json()
     console.log(datos)
-
-
     let div = document.createElement('div');
-    div.innerHTML = `
-    <div class="modal" id="miModal"> 
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5>${response.nameTask}</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-danger">Eliminar</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+    div.innerHTML =
+        `<div class="modal" id="miModal"> 
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+        <h4>${datos.nameTask}</h4> <br>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-danger">Eliminar</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
         </div>
     </div>
+</div>
 </div> `;
-        document.getElementById('container').appendChild(div);
-}
-
-async function getDocUser() {
-    const data = await fetch('http://localHost:1234/task/' + nameUser)
-    const datos = await data.json()
-    const docUser = datos[0].docUser
-    return docUser
-
-}
-
-//Funcion para guardar tareas
-async function postTask() {
-
-    const datos = await getDocUser(); //Se la llama la funcion para saber el documento del usaurio con el nombre del mismo
-    const nameTask = document.getElementById('document').value;
-    const descTask = document.getElementById('description').value;
-    const cateTask = document.getElementById('category').value;
-
-    try {
-        await fetch('http://localHost:1234/task/save', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "docUser": datos,
-                "nameTask": nameTask,   
-                "descTask": descTask,
-                "stateTask": 1,
-                "cateTask": cateTask
-            })
-        });
-        location.reload()//Actualizamos el formulario para que cargue de una vez la nueva tarea
-    } catch (error) {
-        console.log('Hubo un error al guardar la tarjeta :('+ error)
-    }
 }
 
 //Esta función es solo para darle un formato de fecha que se trae desde MySQL
@@ -113,22 +118,22 @@ function newDate(date) {
 
 document.addEventListener('DOMContentLoaded', function () {
     getTask()
-    
+
+
 });
 
 btnCreate.addEventListener('click', () => {
     postTask()
 })
 
-function closeSession(){
+function closeSession() {
     var nuevaPestana = window.open("../../index.html", "_blank");
     setTimeout(function () {
         window.open('about:blank', '_self').close();
     }, 2)
 }
 
-btnCerrar.addEventListener('click', ()=>{
+btnCerrar.addEventListener('click', () => {
     closeSession()
 })
-
 
