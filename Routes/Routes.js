d932e2b94fb0e1e854a23ef4b57e936e72d08666
Sebastian <miguel.poveda1220@gmail.com>
2024-03-05 16:32:06 -0500

    cambio necesarios 05/03/2024

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -5,7 +5,7 @@ import { TaskController } from "../Controller/TaskController.js";
 export const createUserRoutes = ({ usuarioModel }) => {
     const userRoutes = Router()
     const UserController = new userController({ usuarioModel })
-
+   
     userRoutes.get("/login/:user/:pass", UserController.verifySession)
     userRoutes.post('/register', UserController.registerUser)
 
@@ -15,7 +15,7 @@ export const createUserRoutes = ({ usuarioModel }) => {
 export const createTaskRoutes = ({ TaskModel }) => {
     const taskRoutes = Router()
     const taskController = new TaskController({ TaskModel })
-
+// taskRoutes.get('/')
     taskRoutes.get("/get/:user", taskController.getTask)
     taskRoutes.post('/save', taskController.createTask)
     taskRoutes.get('/:user' ,taskController.findDoc)
--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-import express, { json } from 'express'
-import { createUserRoutes } from './Routes/UserRoutes.js'
-import { createTaskRoutes } from './Routes/UserRoutes.js';
+import express, { json } from 'express';
+import { createTaskRoutes } from './Routes/Routes.js';
+import { createUserRoutes } from './Routes/Routes.js';
 import { usuarioModel } from './Model/UsuarioModel.js';
 import { corsMiddleware } from './Middleware/Cors.js';
 import { TaskModel } from './Model/TaskModel.js';
+import path from 'path'; // Importa el módulo path
 
 const app = express();
-app.use(json())
-app.use(corsMiddleware())
-app.use('/user', createUserRoutes({ usuarioModel: usuarioModel }))
-app.use('/task', createTaskRoutes({ TaskModel: TaskModel }))
-const PORT = process.env.PORT || 1234
+app.use(json());
+app.use(corsMiddleware());
+app.use('/user', createUserRoutes({ usuarioModel: usuarioModel }));
+app.use('/task', createTaskRoutes({ TaskModel: TaskModel }));
+const PORT = process.env.PORT || 1234;
 app.disable('x-powered-by');
 
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
 
 app.listen(PORT, () => {
-    console.log('This application litening on the port http://localHost:' + PORT)
-})
+    console.log('This application listening on the port http://localhost:' + PORT);
+});
